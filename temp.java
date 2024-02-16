5a5fa1a8e93530f156ed7edc3e287727fc3f0cd7
Sunil <73417492+Azur3-bit@users.noreply.github.com>
2024-02-16 10:50:17 +0000

    day 2 java tutorial shatrunjai thanks

---
+++ temp.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class temp{
+
+    public static void main(String [] args){
+    
+    //   long startTime = System.currentTimeMIlls();
+      int x = 9999996;
+      int n = 10000000;
+      int count = 0; 
+      Scanner sc = new Scanner(System.in);
+      int i = sc.nextInt();
+      boolean isEven = i%2 == 0;
+
+      System.out.println(isEven);
+    //   if(x >= 0){
+    //     if(isEven){
+    //       for (int i = 0; i < n; i = i + 2) {
+    //         count++;
+    //         if (i == x) {
+    //           System.out.println("We found the values at " + "+count+" try);
+    //           break;
+    //         }
+    //       }
+    //   } else {
+    //       for (int i = 1; i < n; i = i + 2) {
+    //         count++; 
+    //         if (i == x) {
+    //           System.out.println("We found the values at  "+count+" + try");
+    //           break;
+    //         }
+    //       }
+    //    }
+    //  }else {
+    //     System.out.println("Oops! we are expecting valid value"); 
+    //   }
+      
+    //   long endTime = System.currentTimeMillis(); 
+    //   System.out.println("Total time :: " + (endTime - startTime) + " ms");
+      
+   }
+   
+   }
+   
+            
+      
+      
